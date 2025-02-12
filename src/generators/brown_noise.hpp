013eb6935c8f9a7359a42c256bc9d472bdd5936d
klewy <cigankov807@gmail.com>
2025-02-12 16:39:40 +0300

    small optimization

--- src/generators/brown_noise.hpp
+++ src/generators/brown_noise.hpp
@@ -8,23 +8,24 @@
 class BrownNoiseGenerator: public GeneratorBase {
 private:
     SoundConfig m_config{};
-    static inline float smoothingFactor = 0.9f;
-    static inline float leakyIntegratorCoefficient = 0.995f;
+    inline static float GAIN = 0.015f;
+    inline static float LOW_PASS = 0.985f;
+
+    std::random_device device;
+    std::normal_distribution<> dist{0.0f, 1.0f}; // Standard deviation for normal distribution
+    std::mt19937_64 engine{device()};
 public:
+
+
     int generateSamples(void *outputBuf, unsigned long frameCount, void* userData) override
     {
         float* out = (float*)outputBuf;
-
-        std::random_device device;
-        std::normal_distribution<> dist(0.0f, 1.0f); // Standard deviation for normal distribution
-        std::mt19937_64 engine(device());
-
         float brownNoise = 0.0f;
         for (auto i = 0; i < frameCount; ++i) {
             float randomSample = dist(engine);
 
-            brownNoise += randomSample * 0.02f;  // Adjust gain for smoother transitions
-            brownNoise = brownNoise * 0.98f;
+            brownNoise += randomSample * GAIN;  // Adjust gain for smoother transitions
+            brownNoise = brownNoise * LOW_PASS;
 
             *out++ = brownNoise * m_config.volume;
         }
